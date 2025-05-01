1250ae053790806f56bfd658c08154cadd924284
Kreeg <31827862+PoseidonCrit@users.noreply.github.com>
2025-05-01 16:33:27 +0200

    Update constants.js

--- src/constants.js
+++ src/constants.js
@@ -28,7 +28,7 @@ const PATTERN_INTENSITY_INPUT_ID = 'pfp-pattern-intensity'; // ID for pattern in
 // Default settings for the script.
 // This object defines the initial state and structure of the configuration.
 const DEFAULT_CONFIG = {
-    version: 21,        // Incremented config version for ReferenceError fix refinement
+    version: 22,        // Incremented config version for fillLogic restructure
     lastMode: 'fixed',
     lastCount: 10,
     lastFixedQty: 1,
